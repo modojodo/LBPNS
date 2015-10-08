6e637ac5b55b884244f245826b10449c260b568c
Umer Hassan <umerhassan94@gmail.com>
2015-10-08 14:42:29 +0500

    Crawling sitemap, filter for Karachi urls only

--- crawler.js
+++ crawler.js
@@ -8,31 +8,52 @@ var cheerio = require("cheerio");
 
 
 var sitemap = "/sitemap.xml"
+var karachi = "/karachi/";
 
 // Provide the URI with protocol (hence the URL), otherwise hostname and path can't be parsed
-var domain_input = process.argv[2];
-var domain = url.parse(domain_input).hostname;
+var domain = url.parse(process.argv[2]).hostname;
 var domainSitemap = domain + sitemap;
 
 
-request("https://eatoye.pk/sitemap.xml", function (error, response, body) {
-
-    if (!error && response.statusCode == 200) {
+request('http://'+domainSitemap, function (error, response, body) {
+    if (error) {
+        if (error.code == 'ENOTFOUND') {
+            console.log("no internet connectivity");
+            //throw "NO INTERNET CONNECTIVITY";
+        }
+        if (error.code === 'ETIMEDOUT') {
+            console.log("connection timedout");
+            //throw "CONNECTION TIMEDOUT";
+        }
+    } else if (!error && response.statusCode == 200) {
+        console.log(body);
         $ = cheerio.load(body);
-        var links ='';
+        var links = '';
         $("loc").each(function () {
             links += $(this).text() + "\n";
         });
-        //fs.writeFile("./links.txt", links, function (err) {
-        //    if (err) {
-        //        return console.log(err);
-        //    }
-        //    console.log("The file was saved!");
-        //});
-        var linksArr =links.split("\n");
-        linksArr.forEach(function(link){
-            console.log(url.parse(link).path);
-        })
+        var linksArr = links.split("\n");
+        var parsed;
+        var filteredLinks = '';
+        for (var i = 0; i < linksArr.length - 1; i++) {
+            parsed = url.parse(linksArr[i]);
+            if (parsed.path.indexOf(karachi) > -1 && parsed.query == null) {
+                if (i != linksArr.length - 1) {
+                    filteredLinks += linksArr[i] + "\n";
+                } else {
+                    filteredLinks += linksArr[i];
+                }
+
+            }
+        }
+        console.log(filteredLinks);
+        fs.writeFile('./links', filteredLinks, function (err) {
+            if (!err) {
+                console.log("filtererd links saved");
+            } else {
+                console.log(err);
+            }
+        });
     }
 });
 
