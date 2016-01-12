d0cb470be32b936d9ce32589b9add2daecaeef9b
Umer Hassan <umerhassan94@gmail.com>
2016-01-12 12:26:48 +0500

    Added two more helper functions that will be used for categorizing preferences

--- helper.js
+++ helper.js
@@ -1,3 +1,7 @@
+var Deal = require('./models/deal'),
+    Preferences = require('./models/preferences');
+
+
 //Filtering duplicates
 function arrayDuplicateRemove(arr) {
     var c = 0;
@@ -11,4 +15,63 @@ function arrayDuplicateRemove(arr) {
     }
     tempArray.sort();
     return tempArray;
+}
+
+
+function createQueryForPreferencesByRestaurant(pref) {
+    return function (callback) {
+        Deal.find({restaurant: pref}, function (err, deals) {
+            if (!err) {
+                var obj = {};
+                obj['restaurant'] = [];
+                obj['cuisines'] = [];
+                obj['restaurant'].push(pref);
+                for (var i = 0; i < deals.length; i++) {
+                    for (var j = 0; j < deals[i].cuisine.length; j++) {
+                        obj['cuisines'].push(deals[i].cuisine[j]);
+                    }
+                }
+                obj['cuisines'] = arrayDuplicateRemove(obj['cuisines']);
+                if (!err) {
+                    callback(null, obj);
+                } else {
+                    console.log('there');
+                    callback(err, null);
+                }
+            } else {
+            }
+        });
+    }
+}
+
+function createQueryForPreferencesByCuisine(pref) {
+    console.log(pref);
+    return function (callback) {
+        Deal.find({cuisine: pref}, function (err, data) {
+            if (!err) {
+                var obj = {};
+                obj['cuisines'] = [];
+                obj['restaurant'] = [];
+                obj['cuisines'].push(pref);
+                for (var i = 0; i < data.length; i++) {
+                    obj['restaurant'].push(data[i].restaurant);
+                }
+                obj['restaurant'] = arrayDuplicateRemove(obj['restaurant']);
+                if (!err) {
+                    callback(null, obj);
+                } else {
+                    console.log('there');
+                    callback(err, null);
+                }
+            } else {
+            }
+        });
+    }
+}
+
+
+module.exports = {
+    arrayDuplicateRemove: arrayDuplicateRemove,
+    createQueryForPreferencesByRestaurant: createQueryForPreferencesByRestaurant,
+    createQueryForPreferencesByCuisine: createQueryForPreferencesByCuisine
 }
