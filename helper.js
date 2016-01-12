1347df52c10d59dfee1852f194ed685e998dfa86
Syed Haziq Hamdani <syedhaziqhamdani@hotmail.com>
2016-01-12 12:09:15 +0500

    This file contains helper functions.
    eg: remove duplicates in array

---
+++ helper.js
@@ -0,0 +1,14 @@
+//Filtering duplicates
+function arrayDuplicateRemove(arr) {
+    var c = 0;
+    var tempArray = [];
+    arr.sort();
+    for (var i = arr.length - 1; i >= 0; i--) {
+        if (arr[i] != tempArray[c - 1]) {
+            tempArray.push(arr[i]);
+            c++;
+        }
+    }
+    tempArray.sort();
+    return tempArray;
+}
